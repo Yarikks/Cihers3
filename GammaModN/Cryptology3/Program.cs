dc3b1992bf9e96357eb8da5c942ff319e7d9a0de
User <jasper18poil@gmail.com>
2019-10-08 18:04:22 +0300

    add gammas

---
+++ GammaModN/Cryptology3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Cryptology3
+{
+    internal class Program
+    {
+        private static readonly string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        private static void Main(string[] args)
+        {
+            string phrase = "This text ciphered";
+            
+            string key = "yaroslavssciphered";
+
+            Console.WriteLine("Encrypt: " + Gamma(phrase, key, alphabet.Length));
+            Console.ReadLine();
+        }
+
+        static string Gamma(string phrase, string key, int N)
+        {
+            
+            string result = "";
+            int[,] table = new int[4, phrase.Length];
+
+            for (int i = 0; i < phrase.Length; i++)
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (Convert.ToString(key[i]).ToLower() == Convert.ToString(alphabet[j]).ToLower())
+                        table[1, i] = j;
+                    if (Convert.ToString(phrase[i]).ToLower() == Convert.ToString(alphabet[j]).ToLower())
+                        table[0, i] = j;
+                }
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                table[2, i] = (table[0, i] + table[1, i]) % N;
+                result += alphabet[table[2, i]];
+            }
+
+            return result;
+        }
+    }
+}
+
